ae0024f5a202a7f6205d7a829d71e3318fe031c9
printfn <printfn@users.noreply.github.com>
2020-08-16 11:25:48 +1200

    Store history in /Users/printfn/.config/fend

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
+mod config_dir;
+
 fn repl_loop() -> i32 {
     // `()` can be used when no completer is required
     let mut rl = Editor::<()>::with_config(
         rustyline::config::Builder::new()
             .history_ignore_space(true)
             .auto_add_history(true)
+            .max_history_size(10000)
             .build(),
     );
-    if rl.load_history("history.txt").is_err() {
-        // No previous history
+    let history_path = config_dir::get_history_file_path();
+    if let Some(history_path) = history_path.clone() {
+        if rl.load_history(history_path.as_path()).is_err() {
+            // No previous history
+        }
     }
     let mut initial_run = true; // set to false after first successful command
     let mut last_command_success = true;
@@ -55,7 +61,11 @@ fn repl_loop() -> i32 {
             }
         }
     }
-    rl.save_history("history.txt").unwrap();
+    if let Some(history_path) = history_path {
+        if rl.save_history(history_path.as_path()).is_err() {
+            // Error trying to save history
+        }
+    }
     if last_command_success {
         0
     } else {
