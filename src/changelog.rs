ffd169bb40a1b4b107a7e5bd5e8fa25ea3f10a0d
printfn <printfn@users.noreply.github.com>
2020-10-06 13:22:16 +1300

    Add unit test for changelog

--- src/changelog.rs
+++ src/changelog.rs
@@ -3,3 +3,14 @@ const CHANGELOG: &str = include_str!("../CHANGELOG.md");
 pub fn get_changelog() -> &'static str {
     CHANGELOG
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_changelog() {
+        let changelog = get_changelog();
+        assert!(changelog.starts_with("# Changelog\n\n## v"));
+    }
+}
