f5102688706ab7911cd7e90d4f58dfa0a4c15792
printfn <printfn@users.noreply.github.com>
2020-10-17 16:13:46 +1300

    wasm: allow arbitrary timeouts

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -11,19 +11,21 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 struct TimeoutInterrupt {
     start: Instant,
+    timeout: u128,
 }
 
 impl TimeoutInterrupt {
-    fn new() -> Self {
+    fn new_with_timeout(timeout: u128) -> Self {
         Self {
             start: Instant::now(),
+            timeout,
         }
     }
 }
 
 impl fend_core::Interrupt for TimeoutInterrupt {
     fn should_interrupt(&self) -> bool {
-        Instant::now().duration_since(self.start).as_millis() > 500
+        Instant::now().duration_since(self.start).as_millis() > self.timeout
     }
 }
 
@@ -33,9 +35,9 @@ pub fn initialise() {
 }
 
 #[wasm_bindgen]
-pub fn evaluate_fend(input: &str) -> String {
+pub fn evaluate_fend_with_timeout(input: &str, timeout: u32) -> String {
     let mut ctx = fend_core::Context::new();
-    let interrupt = TimeoutInterrupt::new();
+    let interrupt = TimeoutInterrupt::new_with_timeout(u128::from(timeout));
     match fend_core::evaluate_with_interrupt(input, &mut ctx, &interrupt) {
         Ok(res) => res.get_main_result().to_string(),
         Err(msg) => format!("Error: {}", msg),
