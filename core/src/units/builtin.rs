dba3d378c8012ebe3cf3c3b5bf00ab9bef403fd3
printfn <printfn@users.noreply.github.com>
2021-04-07 12:40:06 +1200

    Move unit definitions to static array

--- core/src/units/builtin.rs
+++ core/src/units/builtin.rs
@@ -1,25 +1,115 @@
+#[derive(Eq, PartialEq, PartialOrd, Ord, Clone, Copy)]
+struct UnitDef {
+    singular: &'static str,
+    plural: &'static str,
+    definition: &'static str,
+}
+
+/// construct a unit with only a singular name
+const fn s(name: &'static str, definition: &'static str) -> UnitDef {
+    UnitDef {
+        singular: name,
+        plural: name,
+        definition,
+    }
+}
+
+/// construct a unit with only singular and plural names
+const fn p(singular: &'static str, plural: &'static str, definition: &'static str) -> UnitDef {
+    UnitDef {
+        singular,
+        plural,
+        definition,
+    }
+}
+
+const UNIT_DEFS: [UnitDef; 71] = [
+    s("unitless", "=1"),
+    // SI base units
+    p("second", "seconds", "l@!"),
+    p("meter", "meters", "l@!"),
+    p("kilogram", "kilograms", "l@!"),
+    s("kelvin", "l@!"),
+    p("ampere", "amperes", "l@!"),
+    p("mole", "moles", "l@!"),
+    p("candela", "candelas", "l@!"),
+    // SI base unit abbreviations
+    s("s", "s@second"),
+    p("metre", "metres", "l@meter"),
+    s("m", "s@meter"),
+    p("gram", "grams", "l@1/1000 kilogram"),
+    s("g", "s@gram"),
+    s("K", "s@kelvin"),
+    s("\u{b0}K", "=K"),
+    p("amp", "amps", "l@ampere"),
+    s("A", "s@ampere"),
+    s("mol", "s@mole"),
+    s("cd", "s@candela"),
+    // temperature scales (these have special support for conversions)
+    s("celsius", "l@!"),
+    s("\u{b0}C", "celsius"), // degree symbol
+    s("C", "=\u{b0}C"),
+    s("rankine", "l@5/9 K"),
+    s("\u{b0}R", "rankine"),
+    s("fahrenheit", "l@!"),
+    s("\u{b0}F", "fahrenheit"),
+    s("F", "=\u{b0}F"),
+    // bits and bytes
+    p("bit", "bits", "l@!"),
+    s("bps", "s@bits/second"),
+    p("byte", "bytes", "l@8 bits"),
+    s("b", "s@bit"),
+    s("B", "s@byte"),
+    p("octet", "octets", "l@8 bits"),
+    // standard prefixes
+    s("yotta", "lp@1e24"),
+    s("zetta", "lp@1e21"),
+    s("exa", "lp@1e18"),
+    s("peta", "lp@1e15"),
+    s("tera", "lp@1e12"),
+    s("giga", "lp@1e9"),
+    s("mega", "lp@1e6"),
+    s("myria", "lp@1e4"),
+    s("kilo", "lp@1e3"),
+    s("hecto", "lp@1e2"),
+    s("deca", "lp@1e1"),
+    s("deka", "lp@deca"),
+    s("deci", "lp@1e-1"),
+    s("centi", "lp@1e-2"),
+    s("milli", "lp@1e-3"),
+    s("micro", "lp@1e-6"),
+    s("nano", "lp@1e-9"),
+    s("pico", "lp@1e-12"),
+    s("femto", "lp@1e-15"),
+    s("atto", "lp@1e-18"),
+    s("zepto", "lp@1e-21"),
+    s("yocto", "lp@1e-24"),
+    // non-standard prefixes
+    s("quarter", "lp@1/4"),
+    s("semi", "lp@0.5"),
+    s("demi", "lp@0.5"),
+    s("hemi", "lp@0.5"),
+    s("half", "lp@0.5"),
+    s("double", "lp@2"),
+    s("triple", "lp@3"),
+    s("treble", "lp@3"),
+    // binary prefixes
+    s("kibi", "lp@2^10"),
+    s("mebi", "lp@2^20"),
+    s("gibi", "lp@2^30"),
+    s("tebi", "lp@2^40"),
+    s("pebi", "lp@2^50"),
+    s("exbi", "lp@2^60"),
+    s("zebi", "lp@2^70"),
+    s("yobi", "lp@2^80"),
+];
+
 #[allow(clippy::too_many_lines)]
 #[rustfmt::skip::macros(define_units)]
 pub(crate) fn query_unit<'a>(
     ident: &'a str,
     short_prefixes: bool,
 ) -> Option<(&'static str, &'static str, &'static str)> {
-    macro_rules! define_units {
-        (expr $name:literal $expr:literal) => {
-            return Some(($name, $name, $expr))
-        };
-        (expr $s:literal $p:literal $expr:literal) => {
-            return Some(($s, $p, $expr))
-        };
-        (
-            $(($expr_name_s:literal $(/ $expr_name_p:literal)? $expr_def:literal))+
-        ) => {
-            match ident {
-                $($expr_name_s $(| $expr_name_p)? => define_units!(expr $expr_name_s $($expr_name_p)? $expr_def),)+
-                _ => ()
-            }
-        };
-    }
     if short_prefixes {
         match ident {
             "Ki" => return Some(("Ki", "Ki", "sp@kibi")),
@@ -56,86 +146,10 @@ pub(crate) fn query_unit<'a>(
             _ => (),
         }
     }
-    define_units!(
-("unitless"                       "=1")
-// SI units
-("second"/"seconds"               "l@!")
-("meter"/"meters"                 "l@!")
-("kilogram"/"kilograms"           "l@!")
-("kelvin"                         "l@!")
-("ampere"/"amperes"               "l@!")
-("mole"/"moles"                   "l@!")
-("candela"/"candelas"             "l@!")
-
-("s"                              "s@second")
-("metre"/"metres"                 "l@meter")
-("m"                              "s@meter")
-("gram"/"grams"                   "l@1/1000 kilogram")
-("g"                              "s@gram")
-("K"                              "s@kelvin")
-("\u{b0}K"                        "=K")
-("amp"/"amps"                     "l@ampere")
-("A"                              "s@ampere")
-("mol"                            "s@mole")
-("cd"                             "s@candela")
-
-("celsius"                        "l@!")
-("\u{b0}C"                        "celsius") // degree symbol
-("C"                              "=\u{b0}C")
-("rankine"                        "l@5/9 K")
-("\u{b0}R"                        "rankine")
-("fahrenheit"                     "l@!")
-("\u{b0}F"                        "fahrenheit")
-("F"                              "=\u{b0}F")
-
-("bit"/"bits"                     "l@!")
-("bps"                            "s@bits/second")
-("byte"/"bytes"                   "l@8 bits")
-("b"                              "s@bit")
-("B"                              "s@byte")
-("octet"/"octets"                 "l@8 bits")
-
-("yotta"                          "lp@1e24")
-("zetta"                          "lp@1e21")
-("exa"                            "lp@1e18")
-("peta"                           "lp@1e15")
-("tera"                           "lp@1e12")
-("giga"                           "lp@1e9")
-("mega"                           "lp@1e6")
-("myria"                          "lp@1e4")
-("kilo"                           "lp@1e3")
-("hecto"                          "lp@1e2")
-("deca"                           "lp@1e1")
-("deka"                           "lp@deca")
-("deci"                           "lp@1e-1")
-("centi"                          "lp@1e-2")
-("milli"                          "lp@1e-3")
-("micro"                          "lp@1e-6")
-("nano"                           "lp@1e-9")
-("pico"                           "lp@1e-12")
-("femto"                          "lp@1e-15")
-("atto"                           "lp@1e-18")
-("zepto"                          "lp@1e-21")
-("yocto"                          "lp@1e-24")
-
-("quarter"                        "lp@1/4")
-("semi"                           "lp@0.5")
-("demi"                           "lp@0.5")
-("hemi"                           "lp@0.5")
-("half"                           "lp@0.5")
-("double"                         "lp@2")
-("triple"                         "lp@3")
-("treble"                         "lp@3")
-
-("kibi"                           "lp@2^10")
-("mebi"                           "lp@2^20")
-("gibi"                           "lp@2^30")
-("tebi"                           "lp@2^40")
-("pebi"                           "lp@2^50")
-("exbi"                           "lp@2^60")
-("zebi"                           "lp@2^70")
-("yobi"                           "lp@2^80")
-
-    );
+    for def in &UNIT_DEFS {
+        if def.singular == ident || def.plural == ident {
+            return Some((def.singular, def.plural, def.definition));
+        }
+    }
     None
 }
