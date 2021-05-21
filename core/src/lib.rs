4c6fa8208bd43e4180acd730f0f71fbd013a7676
printfn <printfn@users.noreply.github.com>
2021-05-21 12:10:47 +1200

    Release version 0.1.16

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -6,7 +6,7 @@
 #![forbid(clippy::cognitive_complexity)]
 #![forbid(unreachable_pub)]
 #![forbid(elided_lifetimes_in_paths)]
-#![doc(html_root_url = "https://docs.rs/fend-core/0.1.15")]
+#![doc(html_root_url = "https://docs.rs/fend-core/0.1.16")]
 
 mod ast;
 mod date;
@@ -196,7 +196,7 @@ pub fn evaluate_with_interrupt(
 }
 
 const fn get_version_as_str() -> &'static str {
-    "0.1.15"
+    "0.1.16"
 }
 
 /// Returns the current version of `fend-core`.
