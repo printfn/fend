6591f727809fc4cf7efab5a6403da874e8ea62fa
printfn <printfn@users.noreply.github.com>
2022-08-16 22:18:24 +1200

    Set up automatic deployment for Telegram bot

--- telegram-bot/deploy.sh
+++ telegram-bot/deploy.sh
@@ -2,6 +2,10 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
+npm install
+
+echo "exports.TELEGRAM_API_TOKEN = '$TELEGRAM_BOT_API_TOKEN'" >telegram_api_token.js
+
 rm -f lambda_package.zip
 zip -r lambda_package.zip \
     node_modules/ index.js telegram_api_token.js \
