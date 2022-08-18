d3a7c94339db9d667de0823263656b26ff05af27
printfn <printfn@users.noreply.github.com>
2022-08-19 10:53:12 +1200

    Remove version numbers from MSI and MSIX installers

--- windows-msix/build.ps1
+++ windows-msix/build.ps1
@@ -20,6 +20,10 @@ if (Test-Path $PSScriptRoot\fend.msix) {
     Remove-Item -Force $PSScriptRoot\fend.msix
 }
 
+if (Test-Path $PSScriptRoot\fend-windows-x64.msix) {
+    Remove-Item -Force $PSScriptRoot\fend-windows-x64.msix
+}
+
 mkdir $PSScriptRoot\build
 Copy-Item $PSScriptRoot\..\target\release\fend.exe $PSScriptRoot\build
 (Get-Content $PSScriptRoot\AppxManifest.xml).replace('$FEND_VERSION', $Env:FEND_VERSION) | Set-Content $PSScriptRoot\build\AppxManifest.xml
@@ -28,3 +32,5 @@ Copy-Item $PSScriptRoot\..\icon\fend-icon-150.png $PSScriptRoot\build
 
 & "C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe" pack /d $PSScriptRoot\build /p $PSScriptRoot\fend.msix /verbose
 & "C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe" sign /fd SHA256 /a /f $PSScriptRoot\fend-signing-cert.pfx /p $Env:WINDOWS_CERT_PASSWORD $PSScriptRoot\fend.msix
+
+mv $PSScriptRoot\fend.msix $PSScriptRoot\fend-windows-x64.msix
