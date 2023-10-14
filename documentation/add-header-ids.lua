abec6c48e2ad82a5df5c038d81a48afb0b8a8136
MarkosTh09 <markos.theocharis2009@gmail.com>
2023-10-14 09:40:27 +0400

    autogenerate ids for headers

---
+++ documentation/add-header-ids.lua
@@ -0,0 +1,25 @@
+function generateSlug(el)
+    if el.tag == "Header" then
+        local headerText = pandoc.utils.stringify(el.content)
+        local slug = urlify(headerText)
+        el.attr = { id = slug }
+    end
+    return el
+end
+
+function urlify(text)
+    -- Replace non-alphanumeric characters with hyphens
+    text = text:gsub("[^a-zA-Z0-9]", "-")
+    -- Remove extra hyphens
+    text = text:gsub("-+", "-")
+    -- Remove leading and trailing hyphens
+    text = text:gsub("^%-*(.-)%-*$", "%1")
+    -- Convert to lowercase
+    text = text:lower()
+    return text
+end
+
+-- Apply the filter
+return {
+    { Header = generateSlug }
+}
