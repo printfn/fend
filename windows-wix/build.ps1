22d7be536ff7c8f5882d489a0d011d3ac07d739f
printfn <printfn@users.noreply.github.com>
2024-03-07 09:28:31 +0000

    Update to Wix 3.14
    https://github.com/actions/runner-images/issues/9426

--- windows-wix/build.ps1
+++ windows-wix/build.ps1
@@ -4,9 +4,11 @@ if (Test-Path $PSScriptRoot\build) {
 	Remove-Item -Recurse -Force $PSScriptRoot\build
 }
 
+choco install wixtoolset
+
 mkdir $PSScriptRoot\build
 
-& "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" `
+& "C:\Program Files (x86)\WiX Toolset v3.14\bin\candle.exe" `
 	-arch x64 -ext WixUtilExtension `
 	-dVersion="$Env:FEND_VERSION" `
 	-dFendExePath="$PSScriptRoot\..\target\release\fend.exe" `
@@ -14,7 +16,7 @@ mkdir $PSScriptRoot\build
 	-dIconPath="$PSScriptRoot\..\icon\fend-icon.ico" `
 	-o "$PSScriptRoot\build\" "$PSScriptRoot\main.wxs"
 
-& "C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" `
+& "C:\Program Files (x86)\WiX Toolset v3.14\bin\light.exe" `
 	-spdb `
 	-ext WixUIExtension `
 	-ext WixUtilExtension `
