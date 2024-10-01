05c7b35910a89a8a93af7e4e3f28b8f11562aa9c
printfn <printfn@users.noreply.github.com>
2024-10-01 11:13:30 +0000

    Add wasm artifact to lamdba bundle

--- telegram-bot/build.sh
+++ telegram-bot/build.sh
@@ -13,4 +13,4 @@ npm exec -- esbuild --bundle index.ts --outdir=dist --platform=node
 
 rm -f lambda_package.zip
 # don't include package.json because esbuild makes a CJS bundle that won't work if node finds `"type": "module"`
-zip -j lambda_package.zip dist/index.js
+zip -j lambda_package.zip dist/index.js node_modules/fend-wasm-nodejs/fend_wasm_bg.wasm
