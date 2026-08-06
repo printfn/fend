e73fc7bbd0ae870c031ed498cdce05b636798e46
Joshix <joshix.dev@mailbox.org>
2026-08-06 18:01:03 +0200

    improve Date.add and Date.sub
    - Improve speed for both when adding many years worth of days/weeks
    - Date.add now works with the same units Date.sub supports
    - Prevent Year.prev and Year.next from wrapping around or panicing
    - fix panic/overflow when subbing too many years ("@1970-01-01 - (2^63) years" was 1970-01-01)
    - add some more tests

--- core/src/date.rs
+++ core/src/date.rs
@@ -31,7 +31,7 @@ impl Date {
 		let mut days = ms_since_epoch / 86_400_000; // no leap seconds
 		let mut year = Year::new(1970);
 		while days >= year.number_of_days().into() {
-			year = year.next();
+			year = year.next()?;
 			days -= i64::from(year.number_of_days());
 		}
 		let mut month = Month::January;
@@ -47,11 +47,11 @@ impl Date {
 	}
 
 	fn day_of_week(self) -> DayOfWeek {
-		let d1 = (1
-			+ 5 * ((self.year.value() - 1) % 4)
-			+ 4 * ((self.year.value() - 1) % 100)
-			+ 6 * ((self.year.value() - 1) % 400))
-			% 7;
+		let d1 = ((1
+			+ 5 * ((i64::from(self.year.value()) - 1) % 4)
+			+ 4 * ((i64::from(self.year.value()) - 1) % 100)
+			+ 6 * ((i64::from(self.year.value()) - 1) % 400))
+			% 7) as i32;
 		let ms = match self.month {
 			Month::January => (0, 0),
 			Month::February => (3, 3),
@@ -76,65 +76,65 @@ impl Date {
 		}
 	}
 
-	pub(crate) fn next(self) -> Self {
+	pub(crate) fn next(self) -> FResult<Self> {
 		if self.day.value() < Month::number_of_days(self.month, self.year) {
-			Self {
+			Ok(Self {
 				day: Day::new(self.day.value() + 1),
 				month: self.month,
 				year: self.year,
-			}
+			})
 		} else if self.month == Month::December {
-			Self {
+			Ok(Self {
 				day: Day::new(1),
 				month: Month::January,
-				year: self.year.next(),
-			}
+				year: self.year.next()?,
+			})
 		} else {
-			Self {
+			Ok(Self {
 				day: Day::new(1),
 				month: self.month.next(),
 				year: self.year,
-			}
+			})
 		}
 	}
 
-	pub(crate) fn prev(self) -> Self {
+	pub(crate) fn prev(self) -> FResult<Self> {
 		if self.day.value() > 1 {
-			Self {
+			Ok(Self {
 				day: Day::new(self.day.value() - 1),
 				month: self.month,
 				year: self.year,
-			}
+			})
 		} else if self.month == Month::January {
-			Self {
+			Ok(Self {
 				day: Day::new(31),
 				month: Month::December,
-				year: self.year.prev(),
-			}
+				year: self.year.prev()?,
+			})
 		} else {
 			let month = self.month.prev();
-			Self {
+			Ok(Self {
 				day: Day::new(Month::number_of_days(month, self.year)),
 				month,
 				year: self.year,
-			}
+			})
 		}
 	}
 
 	pub(crate) fn diff_months(self, mut months: i64) -> FResult<Self> {
 		let mut result = self;
 		while months >= 12 {
-			result.year = result.year.next();
+			result.year = result.year.next()?;
 			months -= 12;
 		}
 		while months <= -12 {
-			result.year = result.year.prev();
+			result.year = result.year.prev()?;
 			months += 12;
 		}
 		while months > 0 {
 			if result.month == Month::December {
 				result.month = Month::January;
-				result.year = result.year.next();
+				result.year = result.year.next()?;
 			} else {
 				result.month = result.month.next();
 			}
@@ -143,7 +143,7 @@ impl Date {
 		while months < 0 {
 			if result.month == Month::January {
 				result.month = Month::December;
-				result.year = result.year.prev();
+				result.year = result.year.prev()?;
 			} else {
 				result.month = result.month.prev();
 			}
@@ -155,7 +155,7 @@ impl Date {
 			let mut after = result;
 			if after.month == Month::December {
 				after.month = Month::January;
-				after.year = after.year.next();
+				after.year = after.year.next()?;
 			} else {
 				after.month = after.month.next();
 			}
@@ -198,19 +198,92 @@ impl Date {
 		})
 	}
 
+	fn add_days<I: Interrupt>(self, mut num_days: usize, int: &I) -> FResult<Self> {
+		let mut result = self;
+
+		// make sure to be before 29th February to make skipping years work
+		while result.month != Month::January && num_days > 0 {
+			result = result.next()?;
+			num_days -= 1;
+		}
+
+		while let days_in_year = result.year.number_of_days().into()
+			&& num_days >= days_in_year
+		{
+			num_days -= days_in_year;
+
+			result.year = result.year.next()?;
+
+			test_int(int)?;
+		}
+
+		for _ in 0..num_days {
+			test_int(int)?;
+			result = result.next()?;
+		}
+
+		Ok(result)
+	}
+
 	pub(crate) fn add<I: Interrupt>(self, rhs: Value, int: &I) -> FResult<Value> {
 		let rhs = rhs.expect_num()?;
 		if rhs.unit_equal_to("day", int)? {
 			let num_days = rhs.try_as_usize_unit(int)?;
-			let mut result = self;
-			for _ in 0..num_days {
-				test_int(int)?;
-				result = result.next();
-			}
+
+			let result = self.add_days(num_days, int)?;
+
+			Ok(Value::Date(result))
+		} else if rhs.unit_equal_to("week", int)? {
+			let num_weeks = rhs.try_as_usize_unit(int)?;
+
+			let num_days = num_weeks.checked_mul(7).ok_or(FendError::ValueTooLarge)?;
+
+			let result = self.add_days(num_days, int)?;
+
+			debug_assert_eq!(self.day_of_week(), result.day_of_week());
+
+			Ok(Value::Date(result))
+		} else if rhs.unit_equal_to("month", int)? {
+			let num_months = rhs.try_as_usize_unit(int)?;
+			let result =
+				self.diff_months(i64::try_from(num_months).map_err(|_| FendError::ValueTooLarge)?)?;
+			Ok(Value::Date(result))
+		} else if rhs.unit_equal_to("year", int)? {
+			let num_years = rhs.try_as_usize_unit(int)?;
+			let num_months = num_years.checked_mul(12).ok_or(FendError::ValueTooLarge)?;
+			let result =
+				self.diff_months(i64::try_from(num_months).map_err(|_| FendError::ValueTooLarge)?)?;
 			Ok(Value::Date(result))
 		} else {
 			Err(FendError::ExpectedANumber)
 		}
+	}
+
+	fn sub_days<I: Interrupt>(self, mut num_days: usize, int: &I) -> FResult<Self> {
+		let mut result = self;
+
+		// make sure to be after 29th February to make skipping years work
+		while result.month.as_u8() < Month::March.as_u8() && num_days > 0 {
+			result = result.prev()?;
+			num_days -= 1;
+		}
+
+		while let days_in_year = result.year.number_of_days().into()
+			&& num_days >= days_in_year
+		{
+			num_days -= days_in_year;
+
+			result.year = result.year.prev()?;
+
+			test_int(int)?;
+		}
+
+		for _ in 0..num_days {
+			result = result.prev()?;
+			test_int(int)?;
+		}
+
+		Ok(result)
 	}
 
 	pub(crate) fn sub<I: Interrupt>(self, rhs: Value, int: &I) -> FResult<Value> {
@@ -218,21 +291,17 @@ impl Date {
 
 		if rhs.unit_equal_to("day", int)? {
 			let num_days = rhs.try_as_usize_unit(int)?;
-			let mut result = self;
-			for _ in 0..num_days {
-				result = result.prev();
-				test_int(int)?;
-			}
+
+			let result = self.sub_days(num_days, int)?;
+
 			Ok(Value::Date(result))
 		} else if rhs.unit_equal_to("week", int)? {
 			let num_weeks = rhs.try_as_usize_unit(int)?;
-			let mut result = self;
-			for _ in 0..num_weeks {
-				for _ in 0..7 {
-					result = result.prev();
-				}
-				test_int(int)?;
-			}
+
+			let num_days = num_weeks.checked_mul(7).ok_or(FendError::ValueTooLarge)?;
+
+			let result = self.sub_days(num_days, int)?;
+
 			Ok(Value::Date(result))
 		} else if rhs.unit_equal_to("month", int)? {
 			let num_months = rhs.try_as_usize_unit(int)?;
@@ -241,7 +310,7 @@ impl Date {
 			Ok(Value::Date(result))
 		} else if rhs.unit_equal_to("year", int)? {
 			let num_years = rhs.try_as_usize_unit(int)?;
-			let num_months = num_years * 12;
+			let num_months = num_years.checked_mul(12).ok_or(FendError::ValueTooLarge)?;
 			let result = self
 				.diff_months(-i64::try_from(num_months).map_err(|_| FendError::ValueTooLarge)?)?;
 			Ok(Value::Date(result))
--- core/src/date/year.rs
+++ core/src/date/year.rs
@@ -1,5 +1,6 @@
 use std::{convert, fmt, io};
 
+use crate::num::RangeBound;
 use crate::{
 	error::FendError,
 	result::FResult,
@@ -10,30 +11,51 @@ use crate::{
 pub(crate) struct Year(i32);
 
 impl Year {
-	pub(crate) fn new(year: i32) -> Self {
+	pub(crate) const MAX: Self = Self::new(i32::MAX);
+	pub(crate) const MIN: Self = Self::new(i32::MIN);
+
+	pub(crate) const fn new(year: i32) -> Self {
 		assert!(year != 0, "year 0 is invalid");
 		Self(year)
 	}
 
 	#[inline]
-	pub(crate) fn value(self) -> i32 {
+	pub(crate) const fn value(self) -> i32 {
 		self.0
 	}
 
-	pub(crate) fn next(self) -> Self {
-		if self.value() == -1 {
-			Self::new(1)
-		} else {
-			Self::new(self.value() + 1)
+	pub(crate) fn out_of_range_error(value: i64) -> FendError {
+		FendError::OutOfRange {
+			value: Box::new(value),
+			range: crate::num::Range {
+				start: RangeBound::Closed(Box::new(Self::MIN)),
+				end: RangeBound::Closed(Box::new(Self::MAX)),
+			},
 		}
 	}
 
-	pub(crate) fn prev(self) -> Self {
-		if self.value() == 1 {
+	pub(crate) fn next(self) -> FResult<Self> {
+		Ok(if self.value() == -1 {
+			Self::new(1)
+		} else {
+			Self::new(
+				self.value().checked_add(1).ok_or_else(|| {
+					Self::out_of_range_error(const { Self::MAX.value() as i64 + 1 })
+				})?,
+			)
+		})
+	}
+
+	pub(crate) fn prev(self) -> FResult<Self> {
+		Ok(if self.value() == 1 {
 			Self::new(-1)
 		} else {
-			Self::new(self.value() - 1)
-		}
+			Self::new(
+				self.value().checked_sub(1).ok_or_else(|| {
+					Self::out_of_range_error(const { Self::MIN.value() as i64 - 1 })
+				})?,
+			)
+		})
 	}
 
 	pub(crate) fn is_leap_year(self) -> bool {
@@ -88,7 +110,8 @@ impl fmt::Debug for Year {
 impl fmt::Display for Year {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		if self.value() < 0 {
-			write!(f, "{} BC", -self.0)
+			// cast to bigger int to fix this for Self::MIN
+			write!(f, "{} BC", -i64::from(self.0))
 		} else {
 			write!(f, "{}", self.0)
 		}
@@ -108,5 +131,6 @@ mod tests {
 	#[test]
 	fn negative_year_string() {
 		assert_eq!(Year::new(-823).to_string(), "823 BC");
+		assert_eq!(Year::MIN.to_string(), "2147483648 BC");
 	}
 }
